6700b99def10f8c3c7e67c382727a7067dd862c7
Doğan Derya <doganderya59@gmail.com>
2016-06-23 15:08:36 +0300

    changes

--- src/mailer.js
+++ src/mailer.js
@@ -1,4 +1,4 @@
 const nodeMailer = require('nodemailer');
 const config = require('config');
-const transporter = nodeMailer.createTransport(config.get('mailer.type'), config.get('mailer'));
+const transporter = nodeMailer.createTransport(config.get('mailer'));
 module.exports = transporter;

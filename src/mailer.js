d1bcbadd0d0dd87c3d30ca0c9462636f7e51fd10
Doğan Derya <doganderya59@gmail.com>
2016-06-23 15:15:35 +0300

    :cow2:

--- src/mailer.js
+++ src/mailer.js
@@ -1,4 +1,4 @@
 const nodeMailer = require('nodemailer');
 const config = require('config');
-const transporter = nodeMailer.createTransport(config.get('mailer'));
+const transporter = nodeMailer.createTransport(require('nodemailer-smtp-transport')(config.get('mailer')));
 module.exports = transporter;
